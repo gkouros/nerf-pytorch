bb89e02495c71232750bb3937988f6a741cb7e7e
George Kouros <george.kouros.ece@gmail.com>
2022-03-31 17:35:02 +0200

    Fix condor scripts

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-
+echo 'NeRF job started'
+NAME=$1
 source /users/visics/gkouros/.bashrc
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
@@ -8,12 +8,7 @@ conda activate nerf
 dir=/users/visics/gkouros/projects/nerf-repos/nerf-pytorch/
 cd $dir
 
-SELF=$(readlink -f "${BASH_SOURCE[0]}")
-BASENAME=$(basename "$SELF")
-NAME=${BASENAME/%$'.sh'}
-
-mkdir -p logs/$(NAME)_test
-
-python3 run_nerf.py --config configs/$(NAME).txt
+python3 run_nerf.py --config configs/${NAME}.txt
 
 conda deactivate
+echo 'NeRF job terminated'
