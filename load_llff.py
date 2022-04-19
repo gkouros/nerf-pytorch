12d56d3c87916e510c94421643ed3f467612140d
George Kouros <george.kouros.ece@gmail.com>
2022-04-19 15:09:20 +0200

    Add some documentation

--- load_llff.py
+++ load_llff.py
@@ -6,6 +6,14 @@ import os, imageio
 ##########  see https://github.com/Fyusion/LLFF for original
 
 def _minify(basedir, factors=[], resolutions=[]):
+    """
+    Creates new image dirs with resized images based on given factors and resolutions
+
+    Args:
+        basedir (str): The base directory containing image directories
+        factors (list): Resize factors for new resized images
+        resolutiosn (list): Resolutions for new resized images
+    """
     needtoload = False
     for r in factors:
         imgdir = os.path.join(basedir, 'images_{}'.format(r))
@@ -60,7 +68,19 @@ def _minify(basedir, factors=[], resolutions=[]):
 
 
 def _load_data(basedir, factor=None, width=None, height=None, load_imgs=True):
+    """
+    Loads the images and resizes them to the specified dimensions
 
+    Args:
+        basedir (str): The base directory containing image directories
+        factor (int, optional): Factor for image resizing. Defaults to None.
+        width (int, optional): Width for image resizing. Defaults to None.
+        height (int, optional): Heigh for image resizing. Defaults to None.
+        load_imgs (bool, optional): Specifies whether to load the images. Defaults to True.
+
+    Returns:
+        tuple: Poses, poses bounds, and (optionally) images
+    """
     poses_arr = np.load(os.path.join(basedir, 'poses_bounds.npy'))
     poses = poses_arr[:, :-2].reshape([-1, 3, 5]).transpose([1,2,0])
     bds = poses_arr[:, -2:].transpose([1,0])
@@ -118,14 +138,21 @@ def _load_data(basedir, factor=None, width=None, height=None, load_imgs=True):
     return poses, bds, imgs
 
 
-
-
-
-
 def normalize(x):
+    """
+    Normalize vector to unit vector
+
+    Args:
+        x (array): The vector to normalize
+
+    Returns:
+        array: The normalized vector
+    """
     return x / np.linalg.norm(x)
 
+
 def viewmatrix(z, up, pos):
+    """ Get view transform from vectors z and up and position """
     vec2 = normalize(z)
     vec1_avg = up
     vec0 = normalize(np.cross(vec1_avg, vec2))
@@ -134,23 +161,24 @@ def viewmatrix(z, up, pos):
     return m
 
 def ptstocam(pts, c2w):
+    """ Transform pts from camera to world coordinates """
     tt = np.matmul(c2w[:3,:3].T, (pts-c2w[:3,3])[...,np.newaxis])[...,0]
     return tt
 
 def poses_avg(poses):
-
+    """ Get camera to world transform by averaging camera poses """
     hwf = poses[0, :3, -1:]
-
     center = poses[:, :3, 3].mean(0)
     vec2 = normalize(poses[:, :3, 2].sum(0))
     up = poses[:, :3, 1].sum(0)
     c2w = np.concatenate([viewmatrix(vec2, up, center), hwf], 1)
-
     return c2w
 
 
 
 def render_path_spiral(c2w, up, rads, focal, zdelta, zrate, rots, N):
+    """ Generate poses for spiral path """
+
     render_poses = []
     rads = np.array(list(rads) + [1.])
     hwf = c2w[:,4:5]
@@ -160,11 +188,15 @@ def render_path_spiral(c2w, up, rads, focal, zdelta, zrate, rots, N):
         z = normalize(c - np.dot(c2w[:3,:4], np.array([0,0,-focal, 1.])))
         render_poses.append(np.concatenate([viewmatrix(z, up, c), hwf], 1))
     return render_poses
+<<<<<<< HEAD
+
+=======
+>>>>>>> Add some documentation
 
 
 
 def recenter_poses(poses):
-
+    """ Recenter poses with respect to average pose """
     poses_ = poses+0
     bottom = np.reshape([0,0,0,1.], [1,4])
     c2w = poses_avg(poses)
@@ -182,12 +214,13 @@ def recenter_poses(poses):
 
 
 def spherify_poses(poses, bds):
-
+    """  """
     p34_to_44 = lambda p : np.concatenate([p, np.tile(np.reshape(np.eye(4)[-1,:], [1,1,4]), [p.shape[0], 1,1])], 1)
 
     rays_d = poses[:,:3,2:3]
     rays_o = poses[:,:3,3:4]
 
+    # find scene world center
     def min_line_dist(rays_o, rays_d):
         A_i = np.eye(3) - rays_d * np.transpose(rays_d, [0,2,1])
         b_i = -A_i @ rays_o
@@ -197,23 +230,30 @@ def spherify_poses(poses, bds):
     pt_mindist = min_line_dist(rays_o, rays_d)
 
     center = pt_mindist
+
+    # get up vector of world center
     up = (poses[:,:3,3] - center).mean(0)
 
+    # determine transform from scene center to world frame
     vec0 = normalize(up)
     vec1 = normalize(np.cross([.1,.2,.3], vec0))
     vec2 = normalize(np.cross(vec0, vec1))
     pos = center
     c2w = np.stack([vec1, vec2, vec0, pos], 1)
 
+    # transform poses from world to camera coordinates
     poses_reset = np.linalg.inv(p34_to_44(c2w[None])) @ p34_to_44(poses[:,:3,:4])
 
+    # calculate mean radius of poses from center
     rad = np.sqrt(np.mean(np.sum(np.square(poses_reset[:,:3,3]), -1)))
 
+    # unit scale everything
     sc = 1./rad
     poses_reset[:,:3,3] *= sc
     bds *= sc
     rad *= sc
 
+    # find new center
     centroid = np.mean(poses_reset[:,:3,3], 0)
     zh = centroid[2]
     radcircle = np.sqrt(rad**2-zh**2)
@@ -241,8 +281,9 @@ def spherify_poses(poses, bds):
 
 
 def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=False, path_zflat=False):
+    """ Load data according to LLFF format for forward facing or 360 scenes """
 
-
+    # get poses, near-far bounds, and images
     poses, bds, imgs = _load_data(basedir, factor=factor) # factor=8 downsamples original imgs by 8x
     print('Loaded', basedir, bds.min(), bds.max())
 
@@ -258,14 +299,14 @@ def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=Fal
     poses[:,:3,3] *= sc
     bds *= sc
 
+    # recenter poses around average pose
     if recenter:
         poses = recenter_poses(poses)
 
+    # spherify poses for 360 scenes
     if spherify:
         poses, render_poses, bds = spherify_poses(poses, bds)
-
     else:
-
         c2w = poses_avg(poses)
         print('recentered', c2w.shape)
         print(c2w[:3,:4])
@@ -298,7 +339,6 @@ def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=Fal
 
         # Generate poses for spiral path
         render_poses = render_path_spiral(c2w_path, up, rads, focal, zdelta, zrate=.5, rots=N_rots, N=N_views)
-
 
     render_poses = np.array(render_poses).astype(np.float32)
 
