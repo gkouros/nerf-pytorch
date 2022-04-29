56c350f4169b8b98705a53976c4b0e65b667ac47
George Kouros <george.kouros.ece@gmail.com>
2022-04-29 18:02:36 +0200

    Save each new experiment in a new folder

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo 'NeRF job started'
 NAME=$1
+EXP=$2
 source /users/visics/gkouros/.bashrc
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
@@ -8,7 +9,7 @@ conda activate nerf
 dir=/users/visics/gkouros/projects/nerf-repos/nerf-pytorch/
 cd $dir
 
-python3 run_nerf.py --config configs/${NAME}.txt
+python3 run_nerf.py --config configs/${NAME}.txt --expname "${NAME}/${EXP}"
 
 conda deactivate
 echo 'NeRF job terminated'
