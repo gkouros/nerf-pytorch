78d9d6f9b9cc0f1cbee211c818d3a493db5bda93
George Kouros <george.kouros.ece@gmail.com>
2022-05-02 15:07:52 +0200

    Fix case of experiment name containing slash

--- run_nerf.py
+++ run_nerf.py
@@ -826,7 +826,7 @@ def train():
             with torch.no_grad():
                 rgbs, disps = render_path(render_poses, hwf, K, args.chunk, render_kwargs_test)
             print('Done, saving', rgbs.shape, disps.shape)
-            moviebase = os.path.join(basedir, expname, '{}_spiral_{:06d}_'.format(expname, i))
+            moviebase = os.path.join(basedir, expname, 'spiral_{:06d}_'.format(i))
             imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=8)
             imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=8)
 
