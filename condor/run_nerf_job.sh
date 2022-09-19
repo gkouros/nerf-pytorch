ca86c239db024fc8ae8aa7e6352ebc791713c3a2
George Kouros <george.kouros.ece@gmail.com>
2022-09-19 15:45:44 +0200

    Fix training bash script

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -2,10 +2,13 @@
 echo 'NeRF job started'
 NAME=$1
 EXP=$2
-source /users/visics/gkouros/.bashrc
+
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nerf
+
 dir=/users/visics/gkouros/projects/nerf-repos/nerf-pytorch/
 cd $dir
 
